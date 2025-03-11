80ffdc20a3016a4e07566c97dac4aaa544d58672
helloimlixin <helloimlixin@gmail.com>
2025-03-11 09:02:24 +0000

    launch script fix

--- launch.sh
+++ launch.sh
@@ -1,24 +1,11 @@
-# # #!/bin/bash
-# # export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+#!/bin/bash
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 # Get number of GPUs
 NUM_GPUS=$(nvidia-smi --list-gpus | wc -l)
 
-# # # Launch training using Python's multiprocessing module (works with Hydra)
-# # torchrun \
-# #   --nproc_per_node=$NUM_GPUS \
-# #   run.py \
-# #   model=kronecker_full \
-# #   training=multi_gpu
-
-# #!/bin/bash
-
-# # Get number of GPUs and CPU cores
-# NUM_GPUS=$(nvidia-smi --list-gpus | wc -l)
-# NUM_CORES=$(nproc)
-
-# # Calculate optimal threads per GPU process
-# # Reserve 2 cores for system operations
+# Calculate optimal threads per GPU process
+# Reserve 2 cores for system operations
 AVAILABLE_CORES=$((NUM_CORES - 2))
 THREADS_PER_GPU=$((AVAILABLE_CORES / NUM_GPUS))
 
@@ -27,22 +14,11 @@ if [ $THREADS_PER_GPU -lt 1 ]; then
     THREADS_PER_GPU=1
 fi
 
-# # Set thread-related environment variables
+# Set thread-related environment variables
 export OMP_NUM_THREADS=$THREADS_PER_GPU
-# export MKL_NUM_THREADS=$THREADS_PER_GPU
-# export NUMEXPR_NUM_THREADS=$THREADS_PER_GPU
-# export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
-
-# echo "Running with $NUM_GPUS GPUs, $THREADS_PER_GPU threads per GPU process"
-
-# # Launch with torch distributed
-# torchrun \
-#   --nproc_per_node=$NUM_GPUS \
-#   run.py \
-#   model=kronecker_full \
-#   training=multi_gpu
-
-#!/bin/bash
+export MKL_NUM_THREADS=$THREADS_PER_GPU
+export NUMEXPR_NUM_THREADS=$THREADS_PER_GPU
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 # Common defaults
 MODEL_NAME="meta-llama/Llama-3.2-1B"
@@ -67,12 +43,7 @@ case $MODE in
     echo "Launching standard training..."
     torchrun --nproc_per_node=$NUM_GPUS run.py \
       model=kronecker_full \
-      training=multi_gpu \
-      model.kronecker.strategy=full \
-      model.lora.enabled=true \
-      training.output_dir="$OUTPUT_DIR/kronecker_lora_finetune" \
-      training.dataset_name=glue \
-      training.dataset_subset=mrpc
+      training=multi_gpu
     ;;
 
   *)
